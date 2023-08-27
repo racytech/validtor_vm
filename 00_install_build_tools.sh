971524df8a3b05c9f7ebb83a7f60c13b36cebd80
racytech <racytech0x0@gmail.com>
2023-08-27 21:35:32 +0600

    install lighthouse

--- 00_install_build_tools.sh
+++ 00_install_build_tools.sh
@@ -36,7 +36,18 @@ erigon_github="https://github.com/ledgerwatch/erigon.git"
 printf "\n\nInstalling Erigon... branch=$erigon_branch\n"
 
 git clone $erigon_github
-cd erigon && make erigon
+cd erigon && git checkout $erigon_branch && make erigon
 echo "export PATH=$PATH:$(pwd)/build/bin" >> $HOME/.bashrc
+
+cd $workdir
+
+sleep 1
+lighthouse_branch="unstable"
+lighthouse_github="https://github.com/sigp/lighthouse.git"
+printf "\n\nInstalling Lighthouse... branch=$lighthouse_branch\n"
+
+git clone $lighthouse_github
+
+cd lighthouse && git checkout $lighthouse_branch && make
 
 cd $workdir
