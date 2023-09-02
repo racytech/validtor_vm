0465c9ae289fb5f8f6f3c3ddfd7cadce1895cc47
racytech <racytech0x0@gmail.com>
2023-09-02 18:59:31 +0600

    validator test bg

--- lighthouse_validator.sh
+++ lighthouse_validator.sh
@@ -5,10 +5,22 @@
 source "./common.sh"
 source $HOME/.bashrc
 
+echo "Enter the path to the file containing the password for validator_keys: "
+read pswd_file
 
-lighthouse --network holesky account validator import --directory validator_keys --password-file pswd.txt --reuse-password
+echo $pswd_file
 
-# exec lighthouse \
-#     vc \
-#     --network holesky \
-#     --suggested-fee-recipient "0x4242424242424242424242424242424242424242"
+echo "Enter suggested-fee-recipient: "
+read $suggested_fee_recipient
+
+echo $suggested_fee_recipient
+
+
+lighthouse --network holesky account validator import --directory validator_keys --password-file $pswd_file --reuse-password
+
+# rm $pswd_file
+
+exec lighthouse \
+    vc \
+    --network holesky \
+    --suggested-fee-recipient $suggested_fee_recipient > /dev/null 2>&1
