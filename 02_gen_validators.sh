ca2a2bc58f7d0775c6376df89bca20852a86b9ee
racytech <racytech0x0@gmail.com>
2023-09-02 15:43:24 +0600

    gen_validators test

--- 02_gen_validators.sh
+++ 02_gen_validators.sh
@@ -4,16 +4,25 @@ set -Eeuo pipefail
 
 workdir=$(pwd)
 
+if [ ! -d "./staking-deposit-cli" ]; then
+    echo "Install staking-deposit-cli..."
+    exit 1
+fi
+
+
 declare -i n_validators
 
 echo "Enter mnemonic (the one used for generating pubkeys): "
 read mnemonic
-# echo $mnemonic
+echo $mnemonic
 
 echo -n "Enter number of validators (this has to be equal to the number pubkeys generated using the same mnemonic): "
 read n_validators
-# ((n_validators = n_validators + 1))
+# # ((n_validators = n_validators + 1))
 echo $n_validators
 
 
-eth2-val-tools keystores --source-mnemonic "$mnemonic" --source-min 0 --source-max $n_validators
+
+# $workdir/
+
+# eth2-val-tools keystores --source-mnemonic "$mnemonic" --source-min 0 --source-max $n_validators
