8dc73abd45663e3f55938039f3e03f44187db1ea
racytech <racytech0x0@gmail.com>
2023-09-03 16:53:44 +0600

    export erigon

--- erigon.sh
+++ erigon.sh
@@ -4,11 +4,11 @@
 
 source "./common.sh"
 source "$HOME/.bashrc"
-
+export PATH=$PATH:/home/racytech/validtor_vm/erigon/build/bin
 # exec erigon \
 #     --datadir=$DATA_DIR/erigon \
 #     --chain=holesky > /dev/null 2>&1 &
 
 erigon \
-    --datadir=$DATA_DIR/erigon \
-    --chain=holesky 
+--datadir=$DATA_DIR/erigon \
+--chain=holesky
