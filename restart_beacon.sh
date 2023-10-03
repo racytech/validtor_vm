32e84d5207ca12331d92c600c8c14b891d249afe
racytech <racytech0x0@gmail.com>
2023-10-03 07:35:03 +0600

    add echo

--- restart_beacon.sh
+++ restart_beacon.sh
@@ -5,13 +5,15 @@
 source "./common.sh"
 source "$HOME/.bashrc"
 
+
+
 while true
 do
     pid=$(ps aux | grep 'lighthouse' | grep 'execution-endpoint=http://localhost:8551' | awk '{print $2}')
     if [ -z "$pid" ]; then
-        ./lighthouse_beacon.sh
         date_time=$(date -u)
-        $date_time >> beacon_fails.txt
+        echo $date_time >> beacon_fails.txt
+        ./lighthouse_beacon.sh
         sleep 10 # give sometime for process to start
     fi
     sleep 10
