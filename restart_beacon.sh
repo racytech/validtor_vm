3ddb82b92a4a23a1420b8127ec251aaaacfc9b4e
racytech <racytech0x0@gmail.com>
2023-10-03 07:23:46 +0600

    restart beacon script

---
+++ restart_beacon.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Restart beacon on filure
+
+source "./common.sh"
+source "$HOME/.bashrc"
+
+while true
+do
+    pid=$(ps aux | grep 'lighthouse' | grep 'execution-endpoint=http://localhost:8551' | awk '{print $2}')
+    if [ -z "$pid" ]; then
+        ./lighthouse_beacon.sh
+        sleep 10 # give sometime for process to start
+    fi 
+    sleep 10
+done
+
+
