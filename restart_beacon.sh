27e9f63b86cc1d908d501a9e7985aa76cecda6e6
racytech <racytech0x0@gmail.com>
2023-10-03 07:32:12 +0600

    write date on beacon fail

--- restart_beacon.sh
+++ restart_beacon.sh
@@ -10,9 +10,11 @@ do
     pid=$(ps aux | grep 'lighthouse' | grep 'execution-endpoint=http://localhost:8551' | awk '{print $2}')
     if [ -z "$pid" ]; then
         ./lighthouse_beacon.sh
+        date_time=$(date -u)
+        $date_time >> beacon_fails.txt
         sleep 10 # give sometime for process to start
-    fi 
+    fi
     sleep 10
-done
+done &
 
 
