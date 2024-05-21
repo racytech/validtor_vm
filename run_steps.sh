a739c56c1d2e4376e3593ee66775817cf4e45221
racytech <racytech0x0@gmail.com>
2024-05-21 22:00:49 +0600

    run all steps

--- run_steps.sh
+++ run_steps.sh
@@ -4,5 +4,5 @@
 ./steps/01_install_build_tools.sh
 ./steps/02_install_erigon_lighthouse.sh
 ./steps/03_install_staking_deposit_cli.sh
-# ./steps/04_gen_validators.sh
-# ./steps/05_import_validators.sh
+./steps/04_gen_validators.sh
+./steps/05_import_validators.sh
