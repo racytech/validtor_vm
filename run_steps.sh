c14c48d3ed1c9f562e9df1a02a429574bb8dba6e
racytech <racytech0x0@gmail.com>
2024-05-28 14:32:40 +0600

    run all steps

--- run_steps.sh
+++ run_steps.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # run VM and validator preporation steps
-# ./steps/01_install_build_tools.sh
-# ./steps/02_install_erigon_lighthouse.sh
-# ./steps/03_install_staking_deposit_cli.sh
-# ./steps/04_gen_validators.sh
+./steps/01_install_build_tools.sh
+./steps/02_install_erigon_lighthouse.sh
+./steps/03_install_staking_deposit_cli.sh
+./steps/04_gen_validators.sh
 ./steps/05_import_validators.sh
